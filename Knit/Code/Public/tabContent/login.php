9b013427b4f02367eb04884f6fb5e82e0d23a97d
Isabel Montesanto <imontesanto@LAPTOP-43D1LUI3.localdomain>
2020-11-02 09:06:30 -0800

    Added minimum folders

---
+++ Knit/Code/Public/tabContent/login.php
@@ -0,0 +1,5 @@
+<?php 
+  // Content for the login tab. I had to make a dummy file to create this folder...feel free to rename ofc!
+?>
+
+<p>Login here!</p>
---
+++ Knit/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <link rel = "stylesheet" href = "css/home.css">
+</head>
+<!--Our site's home page!-->
+
+<!--Nav buttons/tabs will go here-->
+
+
+<!--Tab content will be included/required here-->
+<?php include "tabContent/login.php"; ?>
+
+
+</html>

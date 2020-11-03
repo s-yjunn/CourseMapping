a731622c4d0b9fad70b9f2a5f01bd44656e317bd
AllisonBrand <56557237+AllisonBrand@users.noreply.github.com>
2020-11-02 17:34:16 -0800

    Added a table to tab.html.php

---
+++ CourseMapping/Code/php/tab.html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Untitled Pathway</h1>
+<table>
+<tr>
+    <th>First Year</th>
+    <th>Sophomore</th>
+    <th>Junior</th>
+    <th>Senior</th>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+</tr>
+</table>
+</body>
+</html>

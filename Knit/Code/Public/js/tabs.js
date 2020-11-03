7d5ee7ce4bf77071525fba990ae397c83228fb79
Isabel Montesanto <imontesanto@bitbucket.org>
2020-11-02 17:56:42 -0800

    Making tabs dynamic

--- Knit/Code/Public/js/tabs.js
+++ Knit/Code/Public/js/tabs.js
@@ -1,1 +1,16 @@
 // Making the home page's nav tabs dynamic
+
+//This function displays the tab with the id tabName and hides all others in the "tabcontent" class.
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}

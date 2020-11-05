a49b2b8270cddf32d117a0b6ead3d8c1c399bd8e
Bethany Turay <bturay@smith.edu>
2020-11-05 12:28:46 -0500

    messcommit[[[3~

--- Knit/Code/Public/tabContent/vote.php
+++ Knit/Code/Public/tabContent/vote.php
@@ -1,6 +1,36 @@
 <div id="Vote" class="tabcontent">
+<div class="slideshow-container">
 	<div class="card">
 	  <h3>Vote for your favorite design, or submit your own!</h3>
-	  <p>[slideshow]</p> 
-	</div>
+	  <?php
+ $contestants = glob('images/contestants/*'); 
+$numPics = count($contestants);
+if($numPics == 0){
+    echo '<p>There are currently no submissions! Come back later, or submit a design of your own!</p>';    
+    }
+    else{
+        for ($i = 0; $i < $numPics; $i++){
+    echo '<div class="mySlides fade">',
+    '<div class="numbertext">'.($i + 1 ). ' / ' . $numPics .'</div>',
+    '<img src="'.$contestants[$i].'" style="width:100%">';
+        }
+    }
+?> 
 </div>
+</div>
+<?php
+$contestants = glob('images/contestants/*');
+$numPics = count($contestants);
+if($numPics > 0){
+echo '<a class="prev" onclick="plusSlides(-1)">&#10094;</a>',
+'<a class="next" onclick="plusSlides(1)">&#10095;</a>',
+'<div style="text-align:center">';
+
+for ($i = 1; $i <= $numPics; $i++){
+echo '<span class="dot" onclick="currentSlide('.$i.')"></span> ';
+  }
+}
+?>
+  </div>
+
+<script src="js/slides.js"></script>
--- Knit/Code/Public/tabContent/winners.php
+++ Knit/Code/Public/tabContent/winners.php
@@ -1,6 +1,36 @@
 <div id="Winners" class="tabcontent">
+<div class="slideshow-container">
 	<div class="card">
-	  <h3>Your "best of week" winners!</h3>
-	  <p>[slideshow]</p> 
-	</div>
+	  <h3>This Week's Winners!</h3>
+	  <?php
+ $winners = glob('images/winners/*'); 
+$numPics = count($winners);
+if($numPics == 0){
+    echo '<p>There are currently no winners! Please come back later for updates.</p>';    
+    }
+    else{
+        for ($i = 0; $i < $numPics; $i++){
+    echo '<div class="mySlides fade">',
+    '<div class="numbertext">'.($i + 1 ). ' / ' . $numPics .'</div>',
+    '<img src="'.$winners[$i].'" style="width:100%">';
+        }
+    }
+?> 
 </div>
+</div>
+<?php
+$winners = glob('images/winners/*');
+$numPics = count($winners);
+if($numPics > 0){
+echo '<a class="prev" onclick="plusSlides(-1)">&#10094;</a>',
+'<a class="next" onclick="plusSlides(1)">&#10095;</a>',
+'<div style="text-align:center">';
+
+for ($i = 1; $i <= $numPics; $i++){
+echo '<span class="dot" onclick="currentSlide('.$i.')"></span> ';
+  }
+}
+?>
+  </div>
+
+<script src="js/slides.js"></script>

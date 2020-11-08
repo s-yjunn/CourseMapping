3ffe5e3a549614bd5dc32750fa58f44a2ea59ae6
Imane <iberrada@smith.edu>
2020-11-08 21:43:59 +0100

    New Book Changes

--- BookXMerch/Code/Private/Books/collection.php
+++ BookXMerch/Code/Private/Books/collection.php
@@ -23,39 +23,44 @@
     <img class="imageClass" src="Images/b&m.png" alt="bm">
 
     <div class="newFont">
-      <a href="#" class="barCol buttonClass" style="width: 200px; font-family"><b>Bookstore</b>x<b>Merch</b></a>
-      <a href="#" class="barCol buttonClass" onclick="showCollection('comics')">Comic Books</a>
-      <a href="#" class="barCol buttonClass" onclick="showCollection('children')">Children's Books</a>
-      <a href="#" class="barCol buttonClass" onclick="showCollection('scifi')">Science Fiction</a>
-      <a href="#" class="barCol buttonClass" onclick="showCollection('fiction')">Fiction</a>
-      <a href="#" class="barCol buttonClass" onclick="showCollection('nonfiction')">Nonfiction</a>
-      <a href="#" class="barCol buttonClass" onclick="showCollection('debutnovel')">Debut Novel</a>
-      <a href="#" class="barCol buttonClass" onclick="showCollection('horror')">Horror</a>
-      <a href="#" class="barCol buttonClass" onclick="showCollection('romance')">Romance</a>
+      <button class="barCol buttonClass" onclick="showCollection()" style="width: 200px; font-family"><b>Bookstore</b>x<b>Merch</b></button>
+      <button class="barCol buttonClass" onclick="getBookByGenre('comics')">Comic Books</button>
+      <button class="barCol buttonClass" onclick="getBookByGenre('children')">Children's Books</button>
+      <button class="barCol buttonClass" onclick="getBookByGenre('sci-fi')">Science Fiction</button>
+      <button class="barCol buttonClass" onclick="getBookByGenre('fiction')">Fiction</button>
+      <button class="barCol buttonClass" onclick="getBookByGenre('nonfiction')">Nonfiction</button>
+      <button class="barCol buttonClass" onclick="getBookByGenre('debut-novel')">Debut Novel</button>
+      <button class="barCol buttonClass" onclick="getBookByGenre('horror')">Horror</button>
+      <button class="barCol buttonClass" onclick="getBookByGenre('romance')">Romance</button>
     </div>
   </div>
 
 
-  <div class="center">
+  <div id="books" class="center">
+    <div id="wrapper" class="wrapper">
+    </div>
+  </div>
+
+  <div id="original"> 
     <div class="wrapper">
-      <div id="comics"><img class="imageClass"  src="Images/comics.png" alt="bm"></div>
-      <div id="children"><img class="imageClass" src="Images/children.png" alt="bm"></div>
-      <div id="sci-fi"><img class="imageClass" src="Images/scifi.png" alt="bm"></div>
-      <div id="fiction"><img class="imageClass" src="Images/fiction.png" alt="bm"></div>
-      <div id="nonfiction"><img class="imageClass" src="Images/nonfiction.png" alt="bm"></div>
-      <div id="debut-novel"><img class="imageClass" src="Images/debutnovel.png" alt="bm"></div>
-      <div id="horror"><img class="imageClass" src="Images/horror.png" alt="bm"></div>
-      <div id="romance"><img class="imageClass" src="Images/romance.png" alt="bm"></div>
+
+      <div id="comics"> <button class="imgButton" onclick="getBookByGenre('comics')"><img class="imageClass"  src="Images/comics.png" alt="bm"></button></div>
+      <div id="children"> <button class="imgButton" onclick="getBookByGenre('children')"> <img class="imageClass" src="Images/children.png" alt="bm"></button></div>
+      <div id="sci-fi"> <button class="imgButton" onclick="getBookByGenre('sci-fi')"><img class="imageClass" src="Images/scifi.png" alt="bm"></button></div>
+      <div id="fiction"> <button class="imgButton" onclick="getBookByGenre('fiction')"><img class="imageClass" src="Images/fiction.png" alt="bm"></button></div>
+      <div id="nonfiction"> <button class="imgButton" onclick="getBookByGenre('nonfiction')"><img class="imageClass" src="Images/nonfiction.png" alt="bm"></button></div>
+      <div id="debut-novel"> <button class="imgButton" onclick="getBookByGenre('debut-novel')"><img class="imageClass" src="Images/debutnovel.png" alt="bm"></button></div>
+      <div id="horror"> <button class="imgButton" onclick="getBookByGenre('horror')"><img class="imageClass" src="Images/horror.png" alt="bm"></button></div>
+      <div id="romance"> <button class="imgButton" onclick="getBookByGenre('romance')"><img class="imageClass" src="Images/romance.png" alt="bm"></button></div>
     </div>
   </div>
-  
+
 </body>
 
 
 </html>
 
-<?php include "php/bookCollection.php";
-?>
+
 
 
 
--- BookXMerch/Code/Private/Books/php/bookCollection.php
+++ BookXMerch/Code/Private/Books/php/bookCollection.php
@@ -1,21 +1,79 @@
 <?php 
-$logFile = "allBooks.json";
+$logFile = "../allBooks.json";
 $fh = fopen($logFile, 'r+') or die("can't open file");
+
+$genre = $_REQUEST['genre'];
 
 $arrayOfBooks = file_get_contents($logFile);
 $booksArray = json_decode($arrayOfBooks, true);
 $byGenre = array();
+
 foreach ($booksArray as $key => $jsons) { 
-  array_push($byGenre, array($jsons["genre"],$jsons["title"]));
+  if($jsons["genre"]==$genre) {
+    $newArray = array($jsons["title"],$jsons["author"],$jsons["illustrator"],$jsons["description"],$jsons["url"],$jsons["rating"],$jsons["reviews"]);
+    array_push($byGenre, $newArray);
+  }
 }
-bookByGenre($byGenre);
 
-function bookByGenre($arrays) {
-  foreach ($arrays as $key) {
+$i = 0;
+foreach ($byGenre as $newArray) {
+  $i ++;
+  echo "<ul> ";
+  echo "<h1>". "Book " . $i . "</h1> <p>" . $newArray[0] ."</p>";
 
-    echo "<script> var div = document.createElement('div'); div.style.color='white'; div.innerHTML =  ". json_encode($key[1]) . "\n" ."; document.getElementById(".json_encode($key[0]) . ").appendChild(div) ";
-    echo "</script>";
-}
+  //AUTHORS
+  echo "<p>". "AUTHOR(S): ";
+  for($j=0; $j<sizeof($newArray[1]); $j++) {
+      if($j != (sizeof($newArray[1]) - 1)) {
+        echo $newArray[1][$j] . " | ";
+      } else {
+        echo $newArray[1][$j];
+      }
+  }
+  echo "</p>";
+
+  //ILLUSTRATORS
+  if(sizeof($newArray[2])>0) {
+    echo "<p>". "ILLUSTRATOR(S): ";
+    for($j=0; $j<sizeof($newArray[2]); $j++) {
+        if($j != (sizeof($newArray[2]) - 1)) {
+          echo $newArray[2][$j] . " | ";
+        } else {
+          echo $newArray[2][$j];
+        }
+    }
+    echo "</p>";
+  }
+  
+  //SUMMARY
+  echo "<p>". "SUMMARY: ";
+
+  echo "<div>" . $newArray[3] . "</div>";
+
+  echo "</p>";
+
+
+  //url
+  echo "<a href=".$newArray[4]."> Read me! </a>";
+   
+
+  //Rating
+  echo "<p>". "RATING: " . $newArray[5] . "/5";
+  echo "</p>";
+
+
+  //REVIEWS
+  echo "<p>". "Reviews: ";
+    for($j=0; $j<sizeof($newArray[6]); $j++) {
+        echo $newArray[6][$j];
+    }
+    echo "<br>";
+    echo "<br>";
+    echo "<a href='#'> Write a review</a>";
+  echo "</p>";
+
+  echo "</ul>";
+  echo "<br>";
 }
 
 ?>

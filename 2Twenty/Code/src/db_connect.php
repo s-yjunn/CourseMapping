2fe438d6f7c3365a28e9f7721ea1d608edb96c2a
Max Mason <mam20@hampshire.edu>
2020-11-09 23:45:46 +0000

    publicly accessible db values

--- 2Twenty/Code/src/db_connect.php
+++ 2Twenty/Code/src/db_connect.php
@@ -1,9 +1,8 @@
 <?php 
-    // all of this removed from repo for security purposes:
-    $server = "";
-    $user = ""; 
-    $password = "";
-    $db = "";
+    $server = "108.53.75.168:3316";
+    $user = "user"; 
+    $password = "password";
+    $db = "store";
 
     $conn = new mysqli($server, $user, $password, $db);
     if($conn -> connect_error) die("Failed to connect.");

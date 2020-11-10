22cdfc11348b2b19e7cf69852985d7dfaa49a5f8
Nukhbah Majid <nukhbamajid@gmail.com>
2020-11-10 13:13:47 -0500

    "initial user management system set up"

--- BookXMerch/Code/Public/index.php
+++ BookXMerch/Code/Public/index.php
@@ -1,6 +1,11 @@
+<?php
+  session_start();
+  $_SESSION["name"] = "Anon"; 
+
+?>
+
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
@@ -16,7 +21,7 @@
     <!-- Navigation bar-->
     <div class="w3-top">
         <div class="w3-bar w3-white w3-wide w3-padding 3w-card">
-            <a href="../General/index.php" class="w3-bar-item w3-button"><b>Bookstore</b>x<b>Merch</b></a>
+            <a href="../Public/index.php" class="w3-bar-item w3-button"><b>Bookstore</b>x<b>Merch</b></a>
             <!-- Float links to the right. Hide them on small screens -->
 
             <div class="w3-right w3-hide-small">
@@ -36,24 +41,26 @@
             </div>
 
             <div class="w3-right w3-hide-small">
-                <a href="#" class="w3-bar-item w3-button">Sign up</a>
+                <a href="SignUp/signup.php" class="w3-bar-item w3-button">Sign up</a>
             </div>
 
             <div class="w3-right w3-hide-small">
-                <a href="../Login/login.php" class="w3-bar-item w3-button">Login</a>
+                <a href="Login/login.php" class="w3-bar-item w3-button">Login</a>
             </div>
         </div>
     </div>
 
     <header class="w3-display-container w3-content w3-wide" style="max-width:1500px; background-color: white;"
         id="home">
-        <img class="w3-image" src="styles/background.jpg" alt="smith" width="1500" height="800">
+        <img class="w3-image" src="../Public/General/styles/background.jpg" alt="smith" width="1500" height="800">
         <div class="w3-display-middle w3-margin-top w3-center">
             <h1 class="w3-xxlarge w3-text-white"><span class="w3-black w3-opacity-min" style="overflow:hidden"><b>Welcome to BookstorexMerch!
                     </b></span>
             </h1>
         </div>
     </header>
+
+    <script src="../Public/General/js/source.js"></script>
 
 </body>
 

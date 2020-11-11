cd139cb94d2cfad62ce3485143b2ec69b9894029
Alexis Kilayko <alexis.kilayko@gmail.com>
2020-11-11 15:23:33 -0500

    Moved jquery script to top

--- Knit/Code/Public/index.php
+++ Knit/Code/Public/index.php
@@ -10,6 +10,8 @@
 	<link rel="stylesheet" href="css/forum.css">
 	<!-- bootstrap stylesheets -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<!-- ajax jquery script-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -51,8 +53,6 @@
 
 <script src="js/tabs.js"></script>
 <script src="js/showHide.js"></script>
-<!-- ajax jquery script-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <!-- fontawesome script -->
 <script src="https://kit.fontawesome.com/4cb3e5d9fa.js" crossorigin="anonymous"></script>
 

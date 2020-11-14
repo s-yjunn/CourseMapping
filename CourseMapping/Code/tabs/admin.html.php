78ffe83ff10523baf49fbb50f30c3e677b73e804
Hyana Kang <51047617+hyana@users.noreply.github.com>
2020-11-14 15:51:09 -0500

    separated javascript from admin.html.php

--- CourseMapping/Code/tabs/admin.html.php
+++ CourseMapping/Code/tabs/admin.html.php
@@ -14,26 +14,5 @@
 
 <p id="demo"></p>
 
-<script>
-function change_myselect(sel) {
-  var obj, dbParam, xmlhttp, myObj, x, txt = "";
-  obj = { table: sel, limit: 20 };
-  dbParam = JSON.stringify(obj);
-  xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      myObj = JSON.parse(this.responseText);
-      txt += "<table border='1'>"
-      for (x in myObj.EGR) {
-        txt += "<tr><td>" + x + "</td><td>" + x.prereqs + "</td></tr>";
-      }
-      txt += "</table>"    
-      document.getElementById("demo").innerHTML = txt;
-      }
-    };
-  xmlhttp.open("POST", "json/courses.json", true);
-  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xmlhttp.send("x=" + dbParam);
-}
-</script>
+<script type = "text/javascript" src = "js/jsonConvert.js"></script>
  

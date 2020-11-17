89d5faf8f8c859079f9344c3a756f03a96d3d80b
Alexis Kilayko <alexis.kilayko@gmail.com>
2020-11-16 22:48:46 -0500

    Basic pattern maker functionality

--- Knit/Code/Public/index.php
+++ Knit/Code/Public/index.php
@@ -13,11 +13,13 @@
 <html>
 <head>
 	<!-- stylesheets -->
-	<link rel="stylesheet" href="css/home.css">
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="css/slideshow.css">
+	<link rel="stylesheet" href="css/pattern.css">
 	<link rel="stylesheet" href="css/forum.css">
 	<!-- bootstrap stylesheets -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<!-- ajax jquery script-->
+	<!-- jquery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
--- Knit/Code/Public/tabContent/pattern.php
+++ Knit/Code/Public/tabContent/pattern.php
@@ -1,6 +1,13 @@
 <div id="Pattern" class="tabcontent">
 	<div class="card">
-	  <h3>Make your own patterns</h3>
-	  <p>Only members can create patterns! Log in or sign up to become a member!</p> 
+		<h3>Make your own patterns</h3>
+		<p>Only members can create patterns! Log in or sign up to become a member!</p> 
+		
+		<canvas></canvas> 
+		<br><br>
+		
+		<a id="btn" href=""><button>Save Pattern</button></a>
 	</div>
 </div>
+
+<script src="js/pattern.js"></script>

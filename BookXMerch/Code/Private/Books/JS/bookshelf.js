a234931d4e74e8dbf972a054f0d3bfbd9abce8ae
Imane <iberrada@smith.edu>
2020-11-24 16:31:16 +0100

    edits jQuery

--- BookXMerch/Code/Private/Books/JS/bookshelf.js
+++ BookXMerch/Code/Private/Books/JS/bookshelf.js
@@ -1,15 +1,16 @@
 // CSS Bookshelf JS
+// Modified by BooksxMerch group (CSC220)
 // By Steve Workman - www.steveworkman.com/bookshelf
-// Requires jQuery, works with 1.4+
-function bookshelf() {
+// Requires jQuery
+$(function bookshelf() {
 	var className = '';
 	if (!$(this).hasClass('showBook')) {
 		className = $(this).attr('class');
-		$('.bookshelf .'+className).addClass('showBook');
+		$('.bookshelf .' + className).addClass('showBook');
 	} else {
 		$(this).removeClass('showBook');
 		className = $(this).attr('class');
-		$('.bookshelf .'+className).removeClass('showBook');
+		$('.bookshelf .' + className).removeClass('showBook');
 	}
-  }
+});
 $('.bookshelf dt').click(bookshelf);

e0b983e58e24089c86d7426c9bbe9aa3700b9c81
Alexis Kilayko <alexis.kilayko@gmail.com>
2020-11-24 20:23:49 -0500

    Added palette options

--- Knit/Code/Public/tabContent/pattern.php
+++ Knit/Code/Public/tabContent/pattern.php
@@ -11,6 +11,12 @@
 				<div class="palette" id="blue" onclick="selectColor('blue')"></div>
 				<div class="palette" id="purple" onclick="selectColor('purple')"></div>
 				<div class="palette" id="pink" onclick="selectColor('pink')"></div>
+				<div class="palette" id="brown" onclick="selectColor('brown')"></div>				
+				<div class="palette" id="gray" onclick="selectColor('gray')"></div>				
+				<div class="palette" id="black" onclick="selectColor('black')"></div>
+				<div class="palette" id="white" onclick="selectColor('white')"></div>
+				
+				
 				<input id="colorPicker" type="color" value="#FF00FF">
 			</div>
 			<div class="col-xs-11">

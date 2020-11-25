2bc30185937a80a4e52d3f543859111026688555
Alexis Kilayko <alexis.kilayko@gmail.com>
2020-11-24 20:08:20 -0500

    Grid interface for pattern maker

--- Knit/Code/Public/js/pattern.js
+++ Knit/Code/Public/js/pattern.js
@@ -3,9 +3,9 @@ var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
     mouseX = 0,
     mouseY = 0,
-    width = 500,
-    height = 500,
-    color = "black",
+    width = 501, // 500 + 1 to cover "borders" on right and bottom
+    height = 501,
+    color = "black", // draw in black by default
     mousedown = false;
 	colorpick = document.getElementById('colorPicker');
 	btnclear = document.getElementById('btnClear');
@@ -14,12 +14,41 @@ var canvas = document.getElementById('canvas'),
 canvas.width = width;
 canvas.height = height;
 
+// START GRID DRAW
+// drawing a pixel at a time on the canvas
+function colorPixel(x,y) {
+    context.fillRect(x,y,1,1);
+}
+// determining what color pixel should be 
+function pixColor(x,y) {
+	// every 20th pixel in either x/y direction is gray
+    if ((x%20 < 1)||(y%20 < 1)) {
+      return "#ccc";
+    } else { // otherwise, pixel is white
+      return "white";
+    }
+}
+// drawing the grid
+function drawGrid() {
+    var x, y;
+    for (x = 0; x < canvas.width; x++) { // loop over x coords ("rows")
+		for (y = 0; y < canvas.height; y++) { // loop over y coords ("columns") in each x ("row")
+			var color = pixColor(x,y); // determine what to color pixel
+			context.fillStyle = color; // set color
+			colorPixel(x,y); // color pixel accordingly
+		}
+    }
+}
+// draw the grid on the canvas!
+drawGrid();
+// END GRID DRAW
+
 // COLOR SELECT
-// select from default colors
+// selecting from default color list
 function selectColor(col) {
 	color = col;
 }
-// select from color picker
+// selecting from color picker
 colorpick.addEventListener('input', function(event) {
     color = colorpick.value;
 }, false);
@@ -44,7 +73,7 @@ canvas.addEventListener('mousemove', function(event) {
   }
   draw(); // call draw function
 }, false );
-// check for click
+// set mousedown accordingly if user clicks mouse
 canvas.addEventListener('mousedown', function(event) {
     mousedown = true;
 }, false );
@@ -53,10 +82,11 @@ canvas.addEventListener('mouseup', function(event) {
 }, false );
 
 
-/* BUTTONS */
+// BUTTONS
 // clear canvas
 btnclear.addEventListener('click', function(event){
 	context.clearRect(0, 0, canvas.width, canvas.height);
+	drawGrid(); // redraw grid after clearing canvas;
 }, false);
 // save pattern
 btnsave.addEventListener('click', function(event) {

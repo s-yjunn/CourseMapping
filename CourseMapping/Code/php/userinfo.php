2567e6c86a52aeddd03de410f91757df35637dae
Hyana Kang <51047617+hyana@users.noreply.github.com>
2020-11-25 20:50:09 -0500

    added login bar

--- CourseMapping/Code/php/userinfo.php
+++ CourseMapping/Code/php/userinfo.php
@@ -1,13 +1,17 @@
 <?php
 if(isset($_SESSION['username']) && ($_SESSION['username']!= 'admin')){
-    echo $_SESSION['username']. " is a current user!";
-    echo "<form action = 'php/logout.php'>
+    echo "<div class = 'loginBar'>";
+    echo $_SESSION['username']. " is a current user! <form action = 'php/logout.php'>
     <button class='logout-btn' onclick='clearUser()'>LOG OUT</button></form>";
+    echo "</div>";
 } else if($_SESSION['username']== 'admin'){
-    echo "You are an admin!";
-    echo "<form action = 'php/logout.php'>
+    echo "<div class = 'loginBar'>";
+    echo "You are an admin!<form action = 'php/logout.php'>
     <button class='logout-btn'>LOG OUT</button></form>";
+    echo "</div>";
 } else {
+    echo "<div class = 'loginBar'>";
     echo "You are a guest! If you want to see more features, please login!";
+    echo "</div>";
 }
 ?>

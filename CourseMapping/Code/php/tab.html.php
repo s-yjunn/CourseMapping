df51a1c972246b718b867af5ca90fe2ac96cfe7b
Hyana Kang <51047617+hyana@users.noreply.github.com>
2020-11-27 20:40:35 -0500

    added css for buttons and dropdown

--- CourseMapping/Code/php/tab.html.php
+++ CourseMapping/Code/php/tab.html.php
@@ -10,11 +10,11 @@
 
 <body>
     <h1>Untitled Pathway</h1>
-    <button class="active" onclick="save()">Save</button>
     <div class="main" ondragover="drag_over(event)" ondrop="drop(event)">
 
-        <button onclick="getCourses()">Get Courses</button>
-        <button onclick="getPaths()">Get Paths</button>
+        <button class = "get" onclick="getCourses()">Get Courses</button>
+        <button class = "get" onclick="getPaths()">Get Paths</button>
+        <button class="active" onclick="save()">Save</button>
 
         <p id="jsonInfo"></p>
 

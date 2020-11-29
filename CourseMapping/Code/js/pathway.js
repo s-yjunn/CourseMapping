14ad4f2c5f7970aea6a8d8ab964174e4cd6256a3
HayahStudio <hayahstudio@gmail.com>
2020-11-29 12:58:27 -0800

    Modify warning message in pathway and let pathway change with course constantly. Handle lack of info errors.

--- CourseMapping/Code/js/pathway.js
+++ CourseMapping/Code/js/pathway.js
@@ -1,6 +1,5 @@
-var currentPathway; //Global, stores the javascript object to manipulate, whose string representation is in sessionStorage
-
 // Major chosen by the user
+var showMajors = 0;
 var chosenMajor = "XXX";
 
 // Make courses draggable
@@ -8,10 +7,54 @@ $(function () {
   $(".courseBlock").draggable();
 });
 
+// Get available majors for drop down menu from json file
+function getMajors() {
+  if (showMajors == 0) {
+    $(document).ready(function () {
+      $.getJSON("json/majors.json", function (data) {
+        var major = "";
+        $.each(data, function (key, value) {
+          if (key != "meta") {
+            major += "<option value =" + key + ">" + key + "</option>";
+          }
+        });
+        $("#menu").append(major);
+      });
+    });
+    showMajors = 1;
+  }
+}
+
+// When user select a major, show all required courses
+function selectMajor() {
+  // Clear all existing lines, courseBlocks, and info message
+  var items = $(".courseBlock");
+  for (let i = 0; i < items.length; i++) {
+    items[i].remove();
+  }
+  var lines = $(".arrowLine");
+  for (let i = 0; i < lines.length; i++) {
+    lines[i].remove();
+  }
+  document.getElementById("info").textContent = "";
+  document.getElementById("info").style.display = "none";
+
+  // Reposition table, courseBlock, and svg
+  $("#pathTable").css({ marginTop: "0px" });
+  $("#pathwayContent").css({ marginTop: "0px" });
+  $("#map").css({ marginTop: "0px" });
+
+  // Set chosenMajor to the selected major
+  chosenMajor = $("#menu option:selected").val();
+  getCourses();
+}
+
 // Update the line connection each time a course is dragged
-function lineChange(changedCourse) {
+function lineChange(dragged) {
+  var changedCourse = document.getElementById(dragged);
   var lines = document.getElementsByClassName("arrowLine");
   for (var i = 0; i < lines.length; i++) {
+    // Find lines that are affected by dragging
     if (lines[i].id.includes(changedCourse.id)) {
       changingLine = $("#" + lines[i].id);
       var linkedCourse = document.getElementById(
@@ -25,7 +68,6 @@ function lineChange(changedCourse) {
           .attr("y1", changedCourse.style.top.slice(0, -2))
           .attr("x2", linkedCourse.style.left.slice(0, -2))
           .attr("y2", linkedCourse.style.top.slice(0, -2));
-        console.log(changedCourse.style.top + " " + changedCourse.style.left);
 
         // If the course dragged has a prereq, change x2 and y2 of line
       } else {
@@ -48,24 +90,47 @@ function getCourses() {
 
   // Create new course div and make them draggable
   function createCourses(majors, chosenMajor) {
-    var requirements = majors[chosenMajor].major.singular;
+    try {
+      var requirements = majors[chosenMajor].major.singular;
+      // If the major chosen is not found in json file, print a message
+    } catch (error) {
+      var warning = document.createElement("p");
+      var node = document.createTextNode(
+        "Missing information of major " + chosenMajor
+      );
+      warning.appendChild(node);
 
+      var info = document.getElementById("info");
+      info.style.display = "block";
+      info.appendChild(warning);
+      return;
+    }
+
+    // Create the courseBlocks
     for (var i = 0; i < requirements.length; i++) {
       var course = document.createElement("div");
       course.innerHTML = requirements[i];
       course.id = requirements[i].split(" ").join("");
       course.className = "courseBlock ui-widget-content";
-      course.style.top = "" + (i + 2) * 100 + "px";
-      course.style.left = "100px";
-      course.setAttribute("onclick", "lineChange(" + course.id + ")");
+      course.style.top = "" + (i + 2) * 30 + "px";
+      course.style.left = "40px";
       $("#pathwayContent").append(course);
       course.style.display = "block";
     }
 
     $(function () {
-      $(".courseBlock").draggable();
+      $(".courseBlock").draggable({
+        containment: "#pathwayContent",
+        scroll: false,
+        drag: function () {
+          var dragged = $(this).attr("id");
+          lineChange(dragged);
+        },
+      });
     });
   }
+
+  getPaths();
 }
 
 // Create path among existing courses based on their prereq relations
@@ -78,15 +143,35 @@ function getPaths() {
 
   // Create prereq connection between courses
   function createLines(courses) {
-    document.getElementById("info").innerHTML = "Enter line method";
     var userCourses = document.getElementsByClassName("courseBlock");
 
+    // For each course already created on the webpage, check its prereq and create lines accordingly
     for (var i = 0; i < userCourses.length; i++) {
       var department = userCourses[i].innerHTML.substring(0, 3);
-      document.getElementById("info").innerHTML = department;
-      var prereqs = courses[department][userCourses[i].innerHTML].prereqs;
-      document.getElementById("info").innerHTML = prereqs;
 
+      try {
+        var prereqs = courses[department][userCourses[i].innerHTML].prereqs;
+        // If there is no information of prereq for this course, print a message
+      } catch (error) {
+        var warning = document.createElement("p");
+        var node = document.createTextNode(
+          "Missing information of prereq for " + userCourses[i].innerHTML
+        );
+        warning.appendChild(node);
+
+        var info = document.getElementById("info");
+        info.style.display = "block";
+        info.appendChild(warning);
+
+        // Shift table, courseBlock, and svg down accordingly
+        var height = $("#info").height();
+        $("#pathTable").css({ marginTop: "" + height + "px" });
+        $("#pathwayContent").css({ marginTop: "" + height + "px" });
+        $("#map").css({ marginTop: "" + height + "px" });
+        continue;
+      }
+
+      // If there is at least one prereq, draw connection
       if (prereqs.length != 0) {
         var course2 = userCourses[i];
         for (var j = 0; j < prereqs.length; j++) {
@@ -94,12 +179,25 @@ function getPaths() {
 
           // If prereq is not in existing course blocks, print a notice
           if (course1 == null) {
-            document.getElementById("info").innerHTML =
+            var warning = document.createElement("p");
+            var node = document.createTextNode(
               "Prereq " +
-              prereqs[j] +
-              " for " +
-              course2.innerHTML +
-              " is not included in the path.";
+                prereqs[j] +
+                " for " +
+                course2.innerHTML +
+                " is not included in the path."
+            );
+            warning.appendChild(node);
+
+            var info = document.getElementById("info");
+            info.style.display = "block";
+            info.appendChild(warning);
+
+            // Shift table, courseBlock, and svg down accordingly
+            var height = $("#info").height();
+            $("#pathTable").css({ marginTop: "" + height + "px" });
+            $("#pathwayContent").css({ marginTop: "" + height + "px" });
+            $("#map").css({ marginTop: "" + height + "px" });
             continue;
           }
 
@@ -109,8 +207,6 @@ function getPaths() {
             "line"
           );
           newPath.setAttribute("id", course1.id + course2.id);
-          document.getElementById("info").innerHTML =
-            "course 1: " + course1.style.left + " " + course1.style.top;
           newPath.setAttribute("class", "arrowLine");
           newPath.setAttribute("x1", course1.style.left.slice(0, -2));
           newPath.setAttribute("y1", course1.style.top.slice(0, -2));
--- CourseMapping/Code/php/tab.html.php
+++ CourseMapping/Code/php/tab.html.php
@@ -9,57 +9,44 @@
 </head>
 
 <body>
-    <div>
-        <!-- I use a class instead of an id for the pathway title because multiple pathway tabs will be made, and that would be more than one element with the same id. -->
-        <h1 class="pathwayTitle" onclick="titleChange(this)">Untitled Pathway</h1>
-        <!--  This form allows asks the user to change the title of a pathway.  It uses pathwayMeta.js -->
-        <div style="display: none;" class="titleChanger ">
-            <form onsubmit="changeTitle(this); return false;" >
-                <input type="text" name="newTitle" placeholder="New Pathway Title">
-            </form>
-        </div>
-        <button class = "rmButton" onclick="removeTab(currentTab)">Remove</button>
-    </div>
-
+    <h1>Untitled Pathway</h1>
+    <button class="active" onclick="save()">Save</button>
     <div class="main" ondragover="drag_over(event)" ondrop="drop(event)">
 
-        <button class = "get" onclick="getCourses()">Get Courses</button>
-        <button class = "get" onclick="getPaths()">Get Paths</button>
-        <button class="active" onclick="save()">Save</button>
+        <select id="menu" onclick="getMajors()">
+            <option value="None">Select a Major</option>
+        </select>
+        <button onclick="selectMajor()">Get Courses</button>
 
-        <p id="info"></p>
-
-        <div id="pathwayContent">
-            <!-- <div class="canvasTable"> -->
-            <!-- <table class="pathTable">
-                <tr>
-                    <th>First Year</th>
-                    <th>Sophomore</th>
-                    <th>Junior</th>
-                    <th>Senior</th>
-                </tr>
-                <tr>
-                    <td>
-                    </td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </table> -->
-            <!-- </div> -->
-
-            <!-- <div class="canvasTable"> -->
-            <svg id="map" height="300" width="500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <defs>
-                    <marker id="markerArrow" markerWidth="13" markerHeight="13" refX="2" refY="6" orient="auto">
-                        <path d="M2,2 L2,11 L10,6 L2,2" style="fill: rgb(69, 74, 145);" />
-                    </marker>
-                </defs>
-                <circle cx="100" cy="100" r="50" style="stroke: black; fill: red;" />
-            </svg>
-            <!-- </div> -->
+        <div id="info">
         </div>
 
+        <div id="pathwayContent" style="position:absolute;">
+        </div>
+
+        <table id="pathTable">
+            <tr>
+                <th>First Year</th>
+                <th>Sophomore</th>
+                <th>Junior</th>
+                <th>Senior</th>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+
+        <svg id="map" height="300" width="500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <defs>
+                <marker id="markerArrow" markerWidth="13" markerHeight="13" refX="2" refY="6" orient="auto">
+                    <path d="M2,2 L2,11 L10,6 L2,2" style="fill: rgb(69, 74, 145);" />
+                </marker>
+            </defs>
+        </svg>
     </div>
     <div class="side">
 

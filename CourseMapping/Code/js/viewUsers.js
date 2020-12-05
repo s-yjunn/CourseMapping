f9f801c85d807b1b2487f1bedf2d589b32393dc2
Hyana Kang <51047617+hyana@users.noreply.github.com>
2020-12-05 16:52:44 -0500

    fixed the bug in viewUsers.js

--- CourseMapping/Code/js/viewUsers.js
+++ CourseMapping/Code/js/viewUsers.js
@@ -17,26 +17,12 @@ $(document).ready(function () {
                         user += '<td> <input type=\"checkbox\" name = \"user[]\"'; 
                         user += 'id= \"user\" value =\"u_' + value.id + '\"></td>'; 
                         user += '</tr>'; 
-                        $('.test').text(user);},
+                        $('#userTable').append(user); 
+                    },
                     error: function(){
                         alert("fail to load php");
                     }
             }); 
-            user += '<div class = "test"></div>';
         }); 
-        $('#userTable').append(user); 
 }); 
-
-// function fileCount(dir){
-//     var result = false;
-//     $.ajax({ url: 'php/admin/countFiles.php',
-//             data: {'directory': dir},
-//             type: 'POST',
-//             success: function(data) {
-//                 result = data;
-//             return result },
-//             error: function(){
-//                 alert("fail to load php");
-//             }
-//     }); 
-// }
+}); 

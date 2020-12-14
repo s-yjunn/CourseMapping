c2fef92a1cfce60029bbcd04937e993bbca58933
Bethany Turay <bturay@smith.edu>
2020-12-14 14:38:33 -0500

    contest fixes

--- Knit/Code/Public/php/admin/adminContest.php
+++ Knit/Code/Public/php/admin/adminContest.php
@@ -2,9 +2,9 @@
 
 <h4>Contest</h4>
 
-<div class="section">
+<div class="section"  id="section1">
   
-<h5>Approve</h5>
+<h4>Approve</h4>
 <?php 
 
 $comp = file_get_contents("data/contest.json");
@@ -12,117 +12,93 @@ $compData = json_decode($comp, true);
 $currentSubs = $compData["submissions"];
 $numSubs = count($currentSubs);
 
-if($numSubs == 0){
+ 
 
-echo "There are currently no new submissions.";
-
-}
-else{
-
-    echo '<p>Current unapproved submissions:</p>';
-
-echo '<form action="php/moveConts.php" method="post">';
-
-for($i = 0; $i < $numSubs; $i++){
-    echo '<input type="checkbox" name="currentSubs[]" value="'.$currentSubs[$i]["author"]."/".$currentSubs[$i]["title"].'" /> <a href="imgs/contest/'.$currentSubs[$i]["image"].'" target="_blank">'.$currentSubs[$i]["author"]."/".$currentSubs[$i]["title"].'</a><br />';
-}
-
-echo '<br>',
-'<input class="btn1" type="submit" value="Approve">',
-'</form>';
-
-}
-
-?>
+if($numSubs > 0):?>
+        <p>Current unapproved submissions:</p>
+        <ul style="list-style-type:square;">
+     <?php for($i = 0; $i < $numSubs; $i++):?>
+        <li><a href="imgs/contest/<?=$currentSubs[$i]["image"]?>" target="_blank"><?=$currentSubs[$i]["author"]."/".$currentSubs[$i]["title"]?></a><br /></li>
+     <?php endfor; ?>
+     </ul>
+     <p>Please select the submissions you that you would like to appove.</p>
+     <p>Press Ctr + Click to select multiple submissions:</p>
+    <form>
+    <select id="currentSubs" multiple="multiple" size="<?= $numSubs; ?>">
+    <?php for($i = 0; $i < $numSubs; $i++):?>
+        <option value="<?=$currentSubs[$i]["author"]."/".$currentSubs[$i]["title"]?>"> <a href="imgs/contest/<?=$currentSubs[$i]["image"]?>" target="_blank"><?=$currentSubs[$i]["author"]."/".$currentSubs[$i]["title"]?></a><br />
+    <?php  endfor;?>
+    <br>
+    </select></form>
+    <button class="btn1" id="approve">Approve</button>
+    <?php else:?>
+        <p>There are currently no new submissions.</p>
+<?php endif;?>
 
 </div>
 
-<div class="section">
+<div class="section" id="section2">
 
-<h5>Delete</h5>
+<h4>Delete</h4>
+
+<ul style="list-style-type:square;">
+     <?php for($i = 0; $i < $numSubs; $i++):?>
+        <li><a href="imgs/contest/<?=$currentSubs[$i]["image"]?>" target="_blank"><?=$currentSubs[$i]["author"]."/".$currentSubs[$i]["title"]?></a><br /></li>
+     <?php endfor; ?>
+     </ul>
 
 <?php 
 
-if($numSubs > 0){
+if($numSubs > 0):?>
+        <p>Please select the submissions you that you would like to delete:</p>
+        <p>Press Ctr + Click to select multiple submissions:</p>
+    <form>
+    <select id="deleteSubs" multiple="multiple" size="<?= $numSubs; ?>">
+    <?php for($i = 0; $i < $numSubs; $i++):?>
+        <option value="<?=$currentSubs[$i]["author"]."/".$currentSubs[$i]["title"]?>"> <a href="imgs/contest/<?=$currentSubs[$i]["image"]?>" target="_blank"><?=$currentSubs[$i]["author"]."/".$currentSubs[$i]["title"]?></a><br />
+    <?php  endfor;?>
+    <br>
+    </select></form>
+    <button class="btn1" id="delete">Delete</button>
+    <?php else:?>
+        <p>No submissions available. Check again later.</p>
+<?php endif;?>
 
-        echo '<p>Please select the submissions you that you would like to delete:</p>';
-    
-    echo '<form action="php/deleteConts.php" method="post">';
-    
-    for($i = 0; $i < $numSubs; $i++){
-        echo '<input type="checkbox" name="deleteSubs[]" value="'.$currentSubs[$i]["author"]."/".$currentSubs[$i]["title"].'" /> <a href="imgs/contest/'.$currentSubs[$i]["image"].'" target="_blank">'.$currentSubs[$i]["author"]."/".$currentSubs[$i]["title"].'</a><br />';
-    }
-    
-    echo '<br>',
-    '<input class="btn1" type="submit" value="Delete">',
-    '</form>';
-    
-    }
+</div>
+<div class="section" id="section3">
+<h4>Preview Winners</h4>
 
+<?php 
+$comp = file_get_contents("data/contest.json");
+$compData = json_decode($comp, true);
 $currentConts = $compData["contestants"];
 $numCont = count($currentConts);
 
-?>
+if($numCont == 0):?>
 
-</div>
+    <p>There are currently no approved contestants.</p>
 
-<div class="section">
 
-<h5>Preview Winners</h5>
-
-<?php 
-
-if($numCont == 0){
-
-    echo "There are currently no approved contestants.";
-
-}
-else{
-
-    echo "Current contestants:",
-     '<ul style="list-style-type:square;">';
-     for($i = 0; $i < $numCont; $i++){
-        echo '<li><a href="imgs/contest/'.$currentConts[$i]["image"].'" target="_blank">'.$currentConts[$i]["author"]."/".$currentConts[$i]["title"].'</a><br /></li>';
-     }
-     echo '</ul>';     
-     ?>
-
+<?php else:?>
+    <p>Current contestants:</p>
+     <ul style="list-style-type:square;">
+     <?php for($i = 0; $i < $numCont; $i++):?>
+        <li><a href="imgs/contest/<?=$currentConts[$i]["image"]?>" target="_blank"><?=$currentConts[$i]["author"]."/".$currentConts[$i]["title"]?></a><br /></li>
+     <?php endfor; ?>
+     </ul>    
+     <?php endif;
+ if($numCont > 0):?>
 <p>How many winners do you want there to be?</p>
 <form  name="numWin">
 <input type="number" id="numWinners" name="numWinners" min="1" max="<?= $numCont ?>">
 </form>
 <br><br>
 <button class="btn1" id="showWin">Preview Winners</button>
-
-</div>
-    <?php }?>
-<div id="preview" style="display: none;">
-<?php include "php/preview.php"; ?>
-<button class="btn1" id="confirm">Confirm Winners</button>
+<button class="btn1" id="confirm" style="display: none;">Confirm Winners</button>
+<?php endif; ?>
 </div>
 
-<script>
+    
+<div id="preview"></div></div>
 
-$("#showWin").click(function(){
-  var numWinners = document.forms["numWin"]["numWinners"].value;
-$.ajax({
-type: "POST",
-url: "php/moveWin.php",
-data: {numWinners:numWinners},
-success: function(result) {
-    console.log(result);
-                
-}
-})
-$.get("php/preview.php", function(){
-    $("#preview").show();
-  });
-});
-
-$("#confirm").click(function(){
-    $.get("php/reset.php", function(){
-    $("#preview").hide();
-  });
-});
-
-</script>
+<script src="js/contestAdmin.js"></script>

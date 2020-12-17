1179fca6b565592d3cd588dc90cc569e11f6a4f9
Bethany Turay <bturay@smith.edu>
2020-12-16 23:33:53 -0500

    quiz fixes

--- Knit/Code/Public/tabContent/quiz.php
+++ Knit/Code/Public/tabContent/quiz.php
@@ -1,23 +1,26 @@
+<?php 
+/* This is the contents of the quizzes tab
+@author Bethany
+Last modified 12/16/2020 */
+?>
+
 <div id="Quizzes" class="tabcontent">
 	
   <div id="QuizHome">
-  <h3 class="underline">Quizzes</h3>
-  
-  <div id="all" class="refresh">
-  <p id="four" class="quizThumbnail">Take this quiz to test your knitting trivia!</p>
-  <br><br>
-    
-  
+    <h3 class="underline">Quizzes</h3>
+    <!-- holds all quizzes -->
+    <div id="all" class="refresh">
+      <p id="one" class="quizThumbnail">Take this quiz to test your knitting trivia!</p>
+      <br><br>
+    </div>
   </div>
-  </div>
-  
-  
-  
-  
-  <div class="quizContent">
-  <?php include "php/quizzes/trivia.php" ?>
+  <!-- back button -->
+  <img src="imgs/quizzes/backbutton.jpg" alt="back button" id="back" class="backBtnImg"><br><br>
+  <!-- holds all quizzes -->
+  <div class="quizContent" id="quiz">
+    <?php include "php/quizzes/trivia.php" ?>
   </div>
   
-  </div>
+</div>
 
-  <script src="js/quiz.js"></script>
+<script src="js/quiz.js"></script>
